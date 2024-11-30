b0182b8399383d78639acef9dd595724c65ce589
GitHubAvailable <lhzmark@163.com>
2024-11-30 16:39:44 -0500

    feat: add idle state ui functions

---
+++ lib/Display/display.cpp
@@ -0,0 +1,33 @@
+/* display.cpp - Implementation of the UI display functions for
+ * the Embedded Sentry.
+ * License-Identifier: MIT
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://mit-license.org
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "hardware.h"
+#include "display.h"
+
+void start_view()
+{
+    green_led.write(ON);
+}
+
+void idle_view()
+{
+    /* code about the screen */
+}
+
+void recording_view()
+{
+
+}
