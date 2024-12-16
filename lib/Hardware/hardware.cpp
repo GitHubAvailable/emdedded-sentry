767915310ea3f8cf0808b62be91866a1284f45d5
GitHubAvailable <lhzmark@163.com>
2024-12-15 21:40:59 -0500

    feat: create gyro interface fix: adjust global hardware definition

---
+++ lib/Hardware/hardware.cpp
@@ -0,0 +1,111 @@
+/* hardware.cpp - Implementation of the hardware utilities for
+ * the Embedded Sentry.
+ * License-Identifier: MIT
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://mit-license.org
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "hardware.h"
+#include "interrupt.h"
+
+#define SCALING_FACTOR (17.5f * 0.0174532925199432957692236907684886f / 1000.0f)
+
+
+// // The control button of the application.
+// InterruptIn button(BUTTON, PullDown);
+
+// The gyroscope data ready interrupt pin.
+InterruptIn data_ready_int(DATA_READY, PullDown);
+
+// The green LED on the board.
+DigitalOut green_led(GREEN_LED);
+
+// The red LED on the board.
+DigitalOut red_led(RED_LED);
+
+// The ticker used to repeatedly load data.
+Ticker gyro_read_ticker;
+
+Gyroscope Gyroscope::gyro;
+
+Gyroscope::Gyroscope() : gyro_spi(MISO, MOSI, SCK, GYRO_SS, use_gpio_ssel)
+{
+    data_ready_int.rise(&data_ready);
+}
+
+void Gyroscope::set_reg(uint8_t addr, uint8_t val)
+{
+    write_buf[0] = addr;
+    write_buf[1] = val;
+    gyro_spi.transfer(write_buf, 2, read_buf, 2, com_finish);
+    flags.wait_all(GYRO_SPI_COM_FLAG, 5, true);
+}
+
+void Gyroscope::setup()
+{
+    // 8-bit data, idle HIGH, send data at rising edge.
+    gyro_spi.format(8, 3);
+    gyro_spi.frequency(1'000'000);  // clock frequency 1MHz
+
+    // Configure gyroscope.
+    set_reg(GYRO_CTRL1_ADDR, GYRO_CTRL1_CONFIG);
+    set_reg(GYRO_CTRL3_ADDR, GYRO_CTRL3_CONFIG);
+    set_reg(GYRO_CTRL4_ADDR, GYRO_CTRL4_CONFIG);
+}
+
+void Gyroscope::enable()
+{
+    set_reg(GYRO_CTRL1_ADDR, GYRO_ENABLE);
+}
+
+void Gyroscope::disable()
+{
+    set_reg(GYRO_CTRL1_ADDR, GYRO_DISABLE);
+}
+
+void Gyroscope::sleep()
+{
+    set_reg(GYRO_CTRL1_ADDR, GYRO_SLEEP);
+}
+
+void Gyroscope::read(float *buf)
+{
+    write_buf[0] = READ_CMD;
+
+    // Read data.
+    flags.wait_all(GYRO_DATA_READY_FLAG, 20, true);
+    gyro_spi.transfer(write_buf, 7, read_buf, 7, com_finish);
+
+    // Wait for read to complete.
+    // Timeout is set to make it nonblocking. Timeout should not happen.
+    flags.wait_all(GYRO_SPI_COM_FLAG, 80, true);
+
+    uint16_t raw_gx, raw_gy, raw_gz;
+
+    // Extract 16-bit raw data.
+    raw_gx = (read_buf[2] << 8) | read_buf[1];
+    raw_gy = (read_buf[4] << 8) | read_buf[3];
+    raw_gz = (read_buf[6] << 8) | read_buf[5];
+
+    // Load data to the provided buffer.
+    buf[0] = raw_gx * SCALING_FACTOR;
+    buf[1] = raw_gy * SCALING_FACTOR;
+    buf[2] = raw_gz * SCALING_FACTOR;
+
+    return;
+}
+
+// The ILI9341 LCD screen for display.
+LCD_DISCO_F429ZI lcd;
+
+// The ILI9341 LCD touch screen module.
+TS_DISCO_F429ZI ts;
--- lib/Hardware/hardware.h
+++ lib/Hardware/hardware.h
@@ -18,6 +18,7 @@
 
 #include <mbed.h>
 #include <LCD_DISCO_F429ZI.h>
+#include <TS_DISCO_F429ZI.h>
 
 // GPIO macros.
 #define BUTTON PA_0  // user button
@@ -27,30 +28,135 @@
 #define OFF 0
 #define ON 1
 
+// Interrupt pin.
+#define DATA_READY PA_2
+
 // SPI macros.
 #define MISO PF_9
 #define MOSI PF_8
 #define SCK PF_7
 #define GYRO_SS PC_1  // the SS line of the gyroscope
 
+// Gyroscope settings.
+#define CLK_FREQ 1'000'000  // default frequence 1MHz
+#define ENABLE 3
+
+#define GYRO_CTRL1_ADDR 0x20
+#define GYRO_CTRL1_CONFIG 0b00'11'0'0'0'0  // ODR 100Hz, 25 cutoff, xyz enable
+#define GYRO_ENABLE (GYRO_CTRL1_CONFIG | (0b1111))  // enable device
+#define GYRO_DISABLE (GYRO_CTRL1_CONFIG & (~(1 << ENABLE)))
+#define GYRO_SLEEP (GYRO_ENABLE & (~(0b111)))  // turn off all axis, sleep
+
+#define GYRO_CTRL3_ADDR 0x22
+#define GYRO_CTRL3_CONFIG 0b0'0'0'0'1'000  // enable INT2
+
+#define GYRO_CTRL4_ADDR 0x23
+#define GYRO_CTRL4_CONFIG 0b0'0'01'0'00'0  // full-scale selection, 500dps
+
+#define DATA_ADDR 0x28
+#define READ 0x80
+#define AUTO_INCREMENT 0x40
+#define READ_CMD (DATA_ADDR | READ | AUTO_INCREMENT)
+
+// LCD Screen Size
+#define WIDTH 240
+#define HEIGHT 320
+
+// LCD available font sizes.
+#define FONT24 &Font24
+#define FONT20 &Font20
+#define FONT16 &Font16
+#define FONT12 &Font12
+#define FONT8 &Font8
+
 // LCD default color settings.
 #define DEFAULT_BG LCD_COLOR_BLACK
 #define TEXT_DEFAULT_BG LCD_COLOR_BLACK  // backgroud color of texts
 #define TEXT_DEFAULT_COLOR LCD_COLOR_GREEN
 
-// The control button of the application.
-InterruptIn button(BUTTON, PullDown);
+// // The control button of the application.
+// InterruptIn button(BUTTON, PullDown);
+
+// The gyroscope data ready interrupt pin.
+extern InterruptIn data_ready_int;
 
 // The green LED on the board.
-DigitalOut green_led(GREEN_LED);
+extern DigitalOut green_led;
 
 // The red LED on the board.
-DigitalOut red_led(RED_LED);
+extern DigitalOut red_led;
 
-// The SPI master to communicate with I3G4250D gyroscope.
-SPI gyro_spi(MISO, MOSI, SCK, GYRO_SS, use_gpio_ssel);
+// The ticker used to repeatedly load data.
+extern Ticker gyro_read_ticker;
 
-// The ILI9341 LCD screen.
-LCD_DISCO_F429ZI lcd;
+/*!
+    @brief The SPI master to communicate with I3G4250D gyroscope.
+*/
+struct Gyroscope
+{
+    private:
+        uint8_t write_buf[8];
+        uint8_t read_buf[8];
+        SPI gyro_spi;
+
+        /*!
+            @brief Create a gyroscop with pre-defined pin configuration.
+        */
+        Gyroscope();
+
+        /*!
+            @brief Set a specific register on the gyroscope.
+            @param addr the address of the register to be set.
+            @param val the value to be assigned.
+        */
+        void set_reg(uint8_t addr, uint8_t val);
+    
+    public:
+        static Gyroscope gyro;  /*!< a gyroscope singleton for use */
+
+        /*!
+            @brief Set default configuration of I3G4250D gyroscope.
+            @note Should be called with IRQ disabled. This function
+                only loads the default configuration to the gyroscope.
+                `enable()` should be called before using it.
+        */
+        void setup();
+
+        /*!
+            @brief Enable the gyroscope.
+            @warning This function waits and should not be called from
+                interrupt context.
+        */
+        void enable();
+
+        /*!
+            @brief Disable the gyroscope.
+            @warning This function waits and should not be called from
+                interrupt context.
+        */
+        void disable();
+
+        /*!
+            @brief Set the gyroscope to sleep mode.
+            @warning This function waits and should not be called from
+                interrupt context.
+        */
+        void sleep();
+
+        /*!
+            @brief Read angular speed of all 3 axis from the gyroscope.
+            @param buf should at least have length 3, or could cause
+                unexpected behavior
+            @warning This function waits and should not be called from
+                interrupt context.
+        */
+        void read(float *buf);
+};
+
+// The ILI9341 LCD screen for display.
+extern LCD_DISCO_F429ZI lcd;
+
+// The ILI9341 LCD touch screen module.
+extern TS_DISCO_F429ZI ts;
 
 #endif
